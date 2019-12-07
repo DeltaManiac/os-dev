f3e7e73f0eecce055e75d50328e67d41683126bf
DeltaManiac <maxpaynered@gmail.com>
2019-12-07 23:03:18 +0530

    Stacking it up

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,32 +1,32 @@
 ; 
-; Demonstrate addressing
-; Here we store a value and try to use
-; the address to refer to the value
+; Demonstrate stack 
 ;
 mov ah, 0x0e
 
-;Does not resolve the value
-mov al, the_value
+mov bp, 0x8000
+mov sp,bp
+
+; The stack only allows 16 bit values
+; However we push 8bit values as 16-bit values
+; 'A' - 0x00XX
+push 'A'
+push 'B'
+push 'C'
+
+; Since we have pop 16-bit values onto bx
+; we take the lower 8 bits and print those
+pop bx
+mov al,bl
 int 0x10
 
-;Does not resolve the value
-mov al, [the_value]
+pop bx
+mov al,bl
 int 0x10
 
-; Resolves the value as 0x7c00 + offset of the_value
-mov bx, the_value
-add bx, 0x7c00
-mov al,[bx]
-int 0x10
-
-; Resolves the value as 0x7c00 + offset of the_value = 0x7c1e
-; Directly addressing the value
-mov al, [0x7c1e]
+mov al,[0x7ffe]
 int 0x10
 
 jmp $
 
-the_value:
-	db "X"
 times 510-($-$$) db 0
 dw 0xaa55
