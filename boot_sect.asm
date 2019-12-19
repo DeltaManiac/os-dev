96eb6706f51a4bd52735326732d7267197cd0f27
DeltaManiac <maxpaynered@gmail.com>
2019-12-19 21:42:31 +0530

    Printing Hex Strings

--- boot_sect.asm
+++ boot_sect.asm
@@ -2,18 +2,24 @@
 ; A boot sector that prints a string using an external function 
 ;
 
-[org 0x7c00] ; Tell the assembler where the code will be loaded
+;[org 0x7c00] ; Tell the assembler where the code will be loaded
+bits 16
 mov ah, 0x0e ; int  10/ah = 0eh -> scrolling  teletype  BIOS  routine
 
+main:
     mov bx, HELLO_MSG ; We use the BX register as the parameter so we can 
     call print_string  ; Specify the address of the string
  
     mov bx, GOODBYE_MSG
     call print_string
 
+	mov dx, 0x12BC
+	call print_hex
+	call print_string
     jmp $
 
 %include "print_string.asm"
+%include "print_hex.asm"
 
 ; DATA
 HELLO_MSG:
---
+++ makefile
@@ -0,0 +1,17 @@
+build_run: build run
+
+build: 
+	nasm -f elf32 -g3 -F dwarf boot_sect.asm -o main.o
+	ld -Ttext=0x7c00 -melf_i386 main.o -o main.elf
+	objcopy -O binary main.elf main.img
+run:
+	qemu-system-x86_64 -hda main.img
+
+debug:
+	qemu-system-x86_64 -hda main.img -S -s
+
+build_run_bin: build_bin run_bin
+build_bin:
+	nasm boot_sect.asm -f bin -o boot_sect.bin
+run_bin:
+	qemu-system-x86_64 boot_sect.bin
