5b45d7705ff4eead59d53dabc4d671978e66df74
DeltaManiac <maxpaynered@gmail.com>
2020-01-18 09:56:13 +0530

    Basic Kernel

--- boot_sect.asm
+++ boot_sect.asm
@@ -3,22 +3,31 @@
 ;
 
 [org 0x7c00] ; Tell the assembler where the code will be loaded
-
-main:
+KERNEL_OFFSET equ 0x1000
+boot_main:
     mov bp, 0x9000
     mov sp,bp
     mov bx, MSG_REAL_MODE
     call print_string
+	call load_kernel
     call switch_to_pm
     jmp $
 
 %include "./string/print_string.asm"
 %include "./string/print_string_pm.asm"
-; %include "./hex/print_hex.asm"
-; %include "disk_load.asm"
+%include "disk_load.asm"
 %include "gdt.asm"
 %include "switch_pm.asm"
 
+[bits 16]
+load_kernel:
+	mov bx,MSG_LOAD_KERNEL
+	call print_string
+	mov bx,KERNEL_OFFSET
+	mov dh,15
+	mov dl,[BOOT_DRIVE]
+	call disk_load
+	ret
 [bits 32]
 
 BEGIN_PM:
@@ -26,11 +35,15 @@ BEGIN_PM:
      call print_string_pm
      mov bx,MSG_PROT_MODE
      call print_string_pm
+	 call KERNEL_OFFSET
+
      jmp $
 
 
+BOOT_DRIVE db 0
 MSG_REAL_MODE db 'Started in 16 bit real mode',0
 MSG_PROT_MODE db 'Started in 32 bit protected mode',0
+MSG_LOAD_KERNEL db 'Loading kernal into memory',0
 MSG_IN_MODE db '                                                                                ',0
 
 times 510-($-$$) db 0
--- makefile
+++ makefile
@@ -14,5 +14,25 @@ build_run_bin: build_bin run_bin
 build_bin:
 	nasm boot_sect.asm -f bin -o boot_sect.bin
 run_bin:
-	# qemu-system-x86_64 -fda boot_sect.bin 
-	 qemu-system-x86_64  boot_sect.bin 
+	qemu-system-x86_64 -fda boot_sect.bin
+	# qemu-system-x86_64  boot_sect.bin
+
+kernel.bin : kernel_entry.o kernel.o
+	# ld -o kernel.bin -Ttext 0x1000 kernel_entry.o kernel.o --oformat binary
+	#	
+	# $^ is  substituted  with  all of the  target ’s dependancy  files
+	ld -o kernel.bin -Ttext 0x1000 $^ --oformat binary
+
+kernel.o : kernel.c
+	#gcc -ffreestading -c kernel.c -o kernel.o
+	#	
+	# $< is the  first  dependancy  and $@ is the  target  file
+	gcc -ffreestading -c $< -o $@
+
+kernel_entry.o : kernel_entry.asm
+	#nasm kernel_entry.asm -f elf64 -o kernel_entry.o
+	#
+	# $< is the  first  dependancy  and $@ is the  target  file
+	nasm $< -f elf64 -o $@
+
+
